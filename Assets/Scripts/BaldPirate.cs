05079c74a70e2255260cdc4980f1914160c6250c
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-05-10 18:40:26 +0200

    Bald pirate moving

--- Assets/Scripts/BaldPirate.cs
+++ Assets/Scripts/BaldPirate.cs
@@ -6,6 +6,8 @@ public class BaldPirate : MonoBehaviour
 {
     Rigidbody2D body;
     Animator animator;
+    GameObject target;
+
     bool isRunning;
 
     // Start is called before the first frame update
@@ -14,13 +16,20 @@ public class BaldPirate : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
+        target = GameObject.FindWithTag("Player");
+
         StartCoroutine(TestAnim());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isRunning) {
+        var pos = transform.position;
+        var targetPos = target.transform.position;
+
+        if (pos.x > targetPos.x) {
+            body.velocity = new Vector2(-1, body.velocity.y);
+        } else {
             body.velocity = new Vector2(1, body.velocity.y);
         }
 
