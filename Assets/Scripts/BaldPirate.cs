7b81b515a1f9ef8e9f3e8393cbf8f8729176c9a5
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-05-10 17:45:09 +0200

    Added animation transition to bald pirate

--- Assets/Scripts/BaldPirate.cs
+++ Assets/Scripts/BaldPirate.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class BaldPirate : MonoBehaviour
 {
     Rigidbody2D body;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        StartCoroutine(TestAnim());
     }
 
     // Update is called once per frame
     void Update()
     {
         body.velocity = new Vector2(1, body.velocity.y);
+    }
+
+      IEnumerator TestAnim()
+    {
+       
+         yield return new WaitForSeconds(2);
+         animator.SetBool("Running", true);
+
     }
 }
