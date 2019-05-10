951572fbcc3babcdb72088d1c7331f7918581445
Bennie van der Wel <bennievanderwel@gmail.com>
2019-05-10 15:19:14 +0200

    Implemented player movement on x axis

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
     // Update is called once per frame
     void Update()
     {
@@ -17,6 +16,31 @@ public class Player : MonoBehaviour
     }
 
     void PlayerMove()
+    {
+        // Controls
+        moveX = Input.GetAxis("Horizontal");
+        Debug.Log(moveX);
+        // Animation
+        // Direction
+        // if (moveX < 0.0f && facingRight == false)
+        // {
+        //     FlipPlayer();
+        // }
+        // else if (moveX > 0.0f && facingRight == true)
+        // {
+        //     FlipPlayer();
+        // }
+
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void FlipPlayer()
     {
 
     }
