782eed56bdc3eb34cb6a888d00fd18920ef2ae5f
Wilco Schoneveld <schoneveld.wj@gmail.com>
2019-05-10 16:19:34 +0200

    Add bald pirate

---
+++ Assets/Scripts/BaldPirate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaldPirate : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.velocity = new Vector2(1, body.velocity.y);
+    }
+}
