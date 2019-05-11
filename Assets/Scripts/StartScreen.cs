2605f1366522ec9da55f608347807e3d2ad069cc
Krul <nicolas.krul@capgemini.com>
2019-05-11 13:13:12 +0200

    added a start screen, pixel font, script for start screen functions

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+
+    public string preferredNextScene = "Level 1";
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(preferredNextScene, LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
