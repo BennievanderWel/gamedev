1fe7b765cb3f57a91d4d4e99ad93fe18bb3b2168
Krul <nicolas.krul@capgemini.com>
2019-05-12 16:33:43 +0200

    added Game master object, pause screen with functionality

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public bool isPaused = false;
+
+    public Canvas pauseCanvas;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (isPaused == false) {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+            
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseCanvas.enabled = true;
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseCanvas.enabled = false;
+        Time.timeScale = 1;
+    }
+}
